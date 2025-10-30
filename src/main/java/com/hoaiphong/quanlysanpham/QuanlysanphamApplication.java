e0cbfd101c68ff1d2818ca2ef2a7430092ca8802
HoaiPhong <phongnguyenhoai2003@gmail.com>
2025-10-30 09:39:47 +0700

    create base project

---
+++ src/main/java/com/hoaiphong/quanlysanpham/QuanlysanphamApplication.java
@@ -0,0 +1,13 @@
+package com.hoaiphong.quanlysanpham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlysanphamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlysanphamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoaiphong/quanlysanpham/QuanlysanphamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaiphong.quanlysanpham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlysanphamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
