c8a06fb157de56ecc4cd690ded078758d669682e
HoaiPhong <phongnguyenhoai2003@gmail.com>
2025-11-04 08:46:45 +0700

    1

--- src/main/java/com/hoaiphong/quanlysanpham/controller/ProductController.java
+++ src/main/java/com/hoaiphong/quanlysanpham/controller/ProductController.java
@@ -92,6 +92,7 @@ public class ProductController {
         PageResponse<ProductSearchResponse> response = new PageResponse<>();
         response.setData(result.getContent());
         response.setPagination(pagination);
+        System.out.println("1");
         return response;
     }
 
